9fb76b4dbd2a9676528f518de981240e67397ce4
Anwaar <malikanwaar@gmail.com>
2025-04-03 13:27:27 +0100

    =update kernel file

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://anwaarfazil.com','http://anwaarfazil.com'],
 
     'allowed_headers' => ['*'],
 
